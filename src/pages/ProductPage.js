944a60a2c3ae15b16d85abfd44465e1c0b0dc077
seeratalishah <seeratalyshah@gmail.com>
2022-09-11 00:07:31 +0500

    implemented jsx for product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,13 +1,45 @@
-import React from 'react'
-import {useParams} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { products } from "../data/products";
+
+//Filter product from product array
+//Store the product in a state variable
 
 function ProductPage() {
+  const { id } = useParams();
 
-    const {id} = useParams()
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const filteredProduct = products.find((product) => {
+      return product.id === Number(id);
+    });
+
+    setProduct(filteredProduct);
+  }, []);
 
   return (
-    <div>Product {id} </div>
-  )
+    <div className="container">
+      <div className="product-page">
+        <div className="image">
+          <img src={product?.image} alt="" />
+        </div>
+        <div className="product-details">
+          <h2>{product?.title}</h2>
+          <p>{product?.price}</p>
+          <p>{product?.rating}</p>
+
+          <div className="quantity">
+            <button>-</button>
+            <input type="number" />
+            <button>+</button>
+          </div>
+
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProductPage
+export default ProductPage;
