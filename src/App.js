52ee78b0cfe6afbe14159bdf6de4a90183a20140
seeratalishah <seeratalyshah@gmail.com>
2022-09-10 11:53:46 +0500

    Added element to default path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
         <Routes>
-          <Route path='/' ></Route>
+          <Route path='/' element={<HomePage />} ></Route>
         </Routes>
     </div>
   );
