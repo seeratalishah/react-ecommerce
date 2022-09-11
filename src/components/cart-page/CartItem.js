e54105a872c458660da421be763b4ec6606f693b
seeratalishah <seeratalyshah@gmail.com>
2022-09-11 11:11:21 +0500

    added jsx to cart item

--- src/components/cart-page/CartItem.js
+++ src/components/cart-page/CartItem.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function CartItem() {
   return (
-    <div>CartItem</div>
-  )
+    <div className="cart-item">
+      <img src={product.image} alt={product.alt} />
+
+      <div className="product-data">
+        <h2>{product.title}</h2>
+        <p>
+          Price: <span>{product.price} PKR</span>
+        </p>
+
+        <div className="buttons-container">
+          <Link to={`/product/${product.id}`}>View product</Link>
+
+          <button className="cart-btn">
+            <span class="material-icons-outlined">add_shopping_cart</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CartItem
+export default CartItem;
